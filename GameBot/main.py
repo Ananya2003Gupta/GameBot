4ef51c48aa80c6a3fe1405ae445e02b31b3a9cd3
Ananya Gupta <90386813+Ananya2003Gupta@users.noreply.github.com>
2023-02-14 18:45:38 +0530

    Updated game command

--- GameBot/main.py
+++ GameBot/main.py
@@ -12,7 +12,7 @@ def introduce(name):
 
 def start_command(update, context):
     update.message.reply_text(
-        "Type 'game' to start playing the games :). Use '/help' to list the commands"
+        "Type '/game' to start playing the games :). Use '/help' to list the commands"
     )
 
 
@@ -21,9 +21,18 @@ def help_command(update, context):
   /start : To start the bot
   /help  : To get help regarding the commands for the bot
   hey,hello,hi or sup  : Salutations to say hi to bot
-   game  : To start the game
+  /game  : To start the game
   """)
 
+def game_command(update, context):
+  com.item = 0
+  update.message.reply_text("""You can only play the game once!
+To restart the game or play a new game
+Type your response:
+    rock paper scissors
+    hangman 
+    guess the number
+  """)
 
 def handle_message(update, context):
     text = str(update.message.text).lower()
@@ -43,6 +52,8 @@ def main():
     dp.add_handler(CommandHandler("start", start_command))
 
     dp.add_handler(CommandHandler("help", help_command))
+
+    dp.add_handler(CommandHandler("game", game_command))
 
     dp.add_handler(MessageHandler(Filters.text, handle_message))
 
